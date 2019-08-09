5f2dde644e6deff1083a079f20625f1670470661
Thomas Gredig <thomas.gredig@csulb.edu>
2019-08-09 11:39:22 -0700

    fix factor

--- R/ppms.find.Tdep.R
+++ R/ppms.find.Tdep.R
@@ -12,7 +12,7 @@ ppms.find.Tdep <- function(path.raw, sample.name) {
   k = grep('(\\d{2,3})C',f)
   if(length(k)>0) {
     n = gsub('.*\\D(\\d+)C.*','\\1',f[k])
-    Tdep  = sort(factor(n), decreasing = TRUE)[1]
+    Tdep  = as.numeric(n[sort(factor(n), decreasing = TRUE)[1]])
   } else {
     k = grep('\\W+RT',f)
     if (length(k)>0) {
