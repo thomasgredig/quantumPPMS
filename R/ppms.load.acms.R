41e2826c33631eb02986d8bf853d33a0c2affa8a
Thomas Gredig <thomas.gredig@csulb.edu>
2019-09-10 14:13:13 -0700

    Update ppms.load.acms.R

--- R/ppms.load.acms.R
+++ R/ppms.load.acms.R
@@ -10,13 +10,12 @@ empty.frame = data.frame(
   M.edu = c()
 )
 
-#' Loads QD PPMS data from file
+#' Loads QD PPMS ACMS data from file
 #'
 #' @param filename filename including path for PPMS file
-#' @return data frame with 5 data columns: time, T, H, M, Merr
+#' @return data frame with 9 data columns: time, T, M, freq, Ampl, Mdc, M1, M2, Mtot
 #' @examples
-#' filename = system.file("extdata", "20170620_BITHERMAL_SF_VSM_SF170517SI2_MVSH_3K.DAT", package="quantumPPMS")
-#' d = ppms.load(filename)
+#' d = ppms.load.acms(filename)
 #' print(paste('PPMS data loaded with ',nrow(d),'rows.'))
 #' @export
 ppms.load.acms <- function(filename) {
@@ -27,6 +26,8 @@ ppms.load.acms <- function(filename) {
   d = read.csv(filename, skip=20, header=TRUE)
   q = c(2,3,4,5,6,7,9,10,11)
   names(d)[q] =
-    c('time.s','T.K','M.Oe','f.Hz','A.Oe','Mdc.emu','M1.emu','M2.emu','M.edu')
+    c('time.s','T.K','M.Oe','f.Hz','A.Oe','Mdc.emu','M1.emu','M2.emu','M.emu')
+  d[,2]=d[,2]-d[1,2]
+  d$f.Hz = factor(signif(d$f.Hz,3))
   d[,q]
 }
