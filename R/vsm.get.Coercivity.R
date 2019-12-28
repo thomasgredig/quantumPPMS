1a61cfed47e4b01dffc4b3403e11728ce0076223
Thomas Gredig <thomas.gredig@csulb.edu>
2019-12-28 12:00:43 -0800

    retrieve saturation magnetization

--- R/vsm.get.Coercivity.R
+++ R/vsm.get.Coercivity.R
@@ -19,3 +19,25 @@ vsm.get.Coercivity <- function(H, M) {
   spline(M[(k.pos-2):(k.pos+2)],
          H[(k.pos-2):(k.pos+2)], xout=c(0))$y
 }
+
+
+
+#' returns saturation magnetization of hysteresis loops
+#' @param data data frame with H, M, and T
+#' @return matrix with 5 columns, Msat, Msat.sd, intercept, susceptibility, Temeprature
+#' @examples
+#' filename = system.file("extdata", "20170620_BITHERMAL_SF_VSM_SF170517SI2_MVSH_3K.DAT", package="quantumPPMS")
+#' d = ppms.load(filename)
+#' vsm.get.Msat(d)
+#' @export
+vsm.get.Msat <- function(data) {
+  subset(data, (H>0.75*max(data$H) & H<0.95*max(data$H)) | (H< -0.75*max(data$H) & H> -0.95*max(data$H)))-> d3
+  q = c(0,which(diff(d3$time)>5), nrow(d3))
+  d3$loop = factor(rep(1:(length(q)-1),diff(q)))
+  m1= split(d3,d3$loop)
+  t(sapply(m1,function(x) {
+    f=lm(data=x,M~H)$coeff[1:2]
+    mcorr=x$M-x$H*f[2]
+    c(M.sat=mean(mcorr),M.sat.sd=sd(mcorr),f[1],f[2],T=mean(x$T))
+  }))
+}
